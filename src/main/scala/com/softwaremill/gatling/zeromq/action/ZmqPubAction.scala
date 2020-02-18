b352ddea463f5dd2f94c7217970d7f425b19b91e
Michiel Leegwater <mleegwt@users.noreply.github.com>
2020-02-18 21:28:12 +0100

    Update ZmqPubAction.scala

--- src/main/scala/com/softwaremill/gatling/zeromq/action/ZmqPubAction.scala
+++ src/main/scala/com/softwaremill/gatling/zeromq/action/ZmqPubAction.scala
@@ -3,13 +3,15 @@ package com.softwaremill.gatling.zeromq.action
 import com.softwaremill.gatling.zeromq.request.builder.ZmqRequest
 import io.gatling.core.CoreComponents
 import io.gatling.core.action.Action
+import io.gatling.commons.util.Clock
 import org.zeromq.ZMQ
 
 class ZmqPubAction(sock: ZMQ.Socket,
                    request: ZmqRequest,
                    coreComponents: CoreComponents,
                    throttled: Boolean,
-                   next: Action)
+                   next: Action,
+                   clock: Clock)
     extends ZmqAction(sock, request, coreComponents, throttled, next) {
 
   override val name: String = genName("zmqPub")
