8102a71a879cb4081641e8fa564f2600adc61c0b
Michiel Leegwater <mleegwt@users.noreply.github.com>
2020-02-18 21:30:26 +0100

    Update ZmqReqAction.scala

--- src/main/scala/com/softwaremill/gatling/zeromq/action/ZmqReqAction.scala
+++ src/main/scala/com/softwaremill/gatling/zeromq/action/ZmqReqAction.scala
@@ -4,14 +4,16 @@ import com.softwaremill.gatling.zeromq.request.builder.ZmqRequest
 import io.gatling.core.CoreComponents
 import io.gatling.core.action.Action
 import io.gatling.core.session.Session
+import io.gatling.commons.util.Clock
 import org.zeromq.ZMQ
 
 class ZmqReqAction(sock: ZMQ.Socket,
                    request: ZmqRequest,
                    coreComponents: CoreComponents,
                    throttled: Boolean,
-                   next: Action)
-    extends ZmqAction(sock, request, coreComponents, throttled, next) {
+                   next: Action,
+                   clock: Clock)
+    extends ZmqAction(sock, request, coreComponents, throttled, next, clock) {
 
   override val name: String = genName("zmqReq")
 
